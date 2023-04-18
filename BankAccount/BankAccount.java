6d775d51b32a6a75405d4f2bbd0ff1e15ddd5e7f
Diego Piccini <37909339+DiegoPiccinidaTodi@users.noreply.github.com>
2023-04-18 15:48:38 +0200

    Add files via upload

---
+++ BankAccount/BankAccount.java
@@ -0,0 +1,20 @@
+/*
+ * Base3
+ * Author: Diego Piccini da Todi, contact@diegopiccinidatodi.com
+ * April, 2023
+ */
+
+
+public class BankAccount {
+
+    public static void main(String[] args) {
+        double amount;
+
+        amount = 9.46;
+        amount = amount + 46.00;
+
+        System.out.print("This amount of $");
+        System.out.print(amount);
+        System.out.println(" will be added to your account.");
+    }
+}
---
+++ FavouritePainters/FavouritePainters.java
@@ -0,0 +1,12 @@
+/*
+ * Base2
+ * Author: Diego Piccini da Todi, contact@diegopiccinidatodi.com
+ * April, 2023
+ */
+
+public class FavouritePainters {
+
+    public static void main(String[] args) {
+        System.out.println("Turner, Caravaggio, Klimt, Corot");
+    }
+}
