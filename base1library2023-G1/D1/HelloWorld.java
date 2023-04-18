6e2371b1586620b443850b24d49c77355e684be2
Diego Piccini <37909339+DiegoPiccinidaTodi@users.noreply.github.com>
2023-04-18 15:41:51 +0200

    Add files via upload

---
+++ base1library2023-G1/D1/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * Base1
+ * Author: Diego Piccini da Todi, contact@diegopiccinidatodi.com
+ * April, 2023
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ base1library2023-G1/D4/ThePrice.java
@@ -0,0 +1,25 @@
+/*
+ * Base4
+ * Author: Diego Piccini da Todi, contact@diegopiccinidatodi.com
+ * April, 2023
+ */
+
+import java.util.Scanner;
+
+public class ThePrice {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        double amount;
+
+        System.out.print("What's the price of this item, please ? ");
+        amount = keyboard.nextDouble();
+        amount = amount + 148.00;
+
+        System.out.print("Thank you. Please bill me $");
+        System.out.print(amount);
+        System.out.println(" to my personal bank account.");
+
+        keyboard.close();
+    }
+}
